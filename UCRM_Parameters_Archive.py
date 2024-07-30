432550b895169361445eed8a7810dfe9c1626e47
Ansh0369 <kaizenlearning.ansh07@gmail.com>
2024-07-30 16:30:24 +0800

    Create UCRM_Parameters_Archive.py

---
+++ UCRM_Parameters_Archive.py
@@ -0,0 +1,21 @@
+# Databricks notebook source
+# DBTITLE 1,Other Environment
+configSchema = 'th_ucrm_cnfg'
+structuredSchema = 'th_ucrm_strc'
+curatedSchema = 'th_ucrm_crtd'
+consolidatedSchema = 'th_ucrm_cnsl'
+datamartSchema = 'th_ucrm_dmrt'
+
+sourceSystemList = ['LA','IL','GA','LSP']
+baseConfigPath = 'dbfs:/mnt/adls_th/TH/FWD/UCRM'
+ 
+sourceSqlSchema = 'dbo'
+maxPartitionCount = 6
+partitionSize = 1000000
+targetSqlSchema = 'UCRM'
+
+tmpFilePath = 'dbfs:/mnt/adls_th/TH/FWD/UCRM/Working/TempCSVFiles/'
+maximumRowPerFileUpsert = 100000
+maximumRowPerFileDelete = 50
+tmpFilePath = 'dbfs:/mnt/adls_th/TH/FWD/UCRM/Working/TempCSVFiles/'
+maximumRowPerFileArchiveDelete = 40000
